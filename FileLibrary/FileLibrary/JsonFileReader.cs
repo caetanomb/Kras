2209c7009a36054b3f4b0d3d30592ae2b8a9f607
Caetano Marques Bruno <caetanomb@gmail.com>
2018-06-02 20:12:12 +0200

    Version 7

---
+++ FileLibrary/FileLibrary/JsonFileReader.cs
@@ -0,0 +1,31 @@
+﻿using FileLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileLibrary
+{
+    public class JsonFileReader : FileReader, IJsonFileReader
+    {        
+        public JsonFileReader(string filePath, string fileName)
+            : base(filePath, SetExtension(fileName))
+        {
+        }
+
+        /// <summary>
+        /// Sets .json extension whether file does not have an extension or has a different extension
+        /// </summary>
+        /// <param name="fileName">File Name</param>
+        /// <returns></returns>
+        public static string SetExtension(string fileName)
+        {
+            return Path.ChangeExtension(fileName, ".json");
+        }
+
+        public new string Read()
+        {
+            return base.Read().AsString();            
+        }
+    }
+}
---
+++ FileLibrary/UnitTests.Domain/JsonFileReaderTest.cs
@@ -0,0 +1,36 @@
+﻿using FileLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.Domain
+{
+    public class JsonFileReaderTest
+    {
+        [Theory]
+        [InlineData("ContentJson.json")]
+        [InlineData("ContentJson")]
+        public void A_User_Should_be_Able_to_Read_A_JsonFile(string fileName)
+        {
+            string filePath = @"E:\Waes\Kras\Src\FileLibrary\UnitTests.Domain\";
+
+            var jsonFileReader = new JsonFileReader(filePath, fileName);
+            string contentFile = jsonFileReader.Read();
+
+            Assert.Equal("{\"title\": \"Person\",\"type\": \"object\",\"lastName\": {\"type\": \"string\"}}", contentFile);
+        }
+
+        [Theory]
+        [InlineData("ContentJson")]
+        [InlineData("ContentJson.json")]
+        public void Make_Sure_File_Has_TextFile_Extension(string fileName)
+        {
+            string filePath = @"E:\Waes\Kras\Src\FileLibrary\UnitTests.Domain\";
+
+            var jsonFileReader = new JsonFileReader(filePath, fileName);
+
+            Assert.Equal("ContentJson.json", jsonFileReader.Filename);
+        }
+    }
+}
